038baac8b39ed26dcaf8768f3ca54d0cc9dd63a0
Victor van Poppelen <vvanpo@gmail.com>
2015-07-22 16:56:52 -0400

    self.c: initial description of various messages/operations

--- self.c
+++ self.c
@@ -27,6 +27,7 @@ struct self {
 /// static function declarations
 static void load_config(struct self *);
 static void self_listen(struct self *);
+static void parse_message(buffer);
 
 // self_load loads all the necessary data structures into self required for
 // starting the daemon
@@ -59,6 +60,10 @@ void load_config(struct self *self){
 // self_listen opens the configured socket and begins a listener thread to
 // service requests from peers
 void self_listen(struct self *self){
+    //TODO: open a separate UDP socket for exclusive use with bulk file
+    //  tranfers (the result of a successful rq_find_file request)
+    //  this is to ensure that bulk transfers don't crowd out incoming messages
+    //  if it fills the socket buffer
     int sockfd = socket(AF_INET6, SOCK_DGRAM, 0);
     if (sockfd == -1); //error
     in_port_t port = htons(self->config.udp_port);
@@ -66,6 +71,7 @@ void self_listen(struct self *self){
     socklen_t addrlen = sizeof(addr);
     if (bind(sockfd, (struct sockaddr *) &addr, addrlen)); //error
     //TODO: attempt bind with port 0 if denied due to in-use port
+    //TODO: find optimal buffer size
     buffer buf = { malloc(4096), 4096 };
     if (!buf.data); //error
     while (1){
@@ -74,9 +80,50 @@ void self_listen(struct self *self){
         int ret = recvfrom(sockfd, buf.data, buf.length, MSG_TRUNC,
                 (struct sockaddr *) &addr, &addrlen);
         if (ret == -1); //error
-        if (buf.length < ret); //error/increase buffer and request resend
-        
+        if (buf.length < ret); //error
+        parse_message(buf);
     }
     if (close(sockfd)); //error
+}
+
+// if the requestee doesn't have the public key of the requester to
+// verify the signature, it sends back a rq_find_peer request
+struct request {
+    // rq_find_peer requests information about a specific fingerprint
+    //      the response consists of a list of peers close to the requested, or
+    //      a table of info about the peer
+    //      rq_find_peer sent to the peer with that same fingerprint always
+    //      returns a table of info (like the peer's public key)
+    // rq_find_file includes a file's hash, and returns a list of nearby peers,
+    //      or if the requestee knows owners of the file, a list of owners along
+    //      with more nearby peers if there is room left in list_max
+    //      if rq_find_file is sent to an owner of the file, the owner responds
+    //      with a list of more owners/nearby peers just like above, but also
+    //      sets up a bulk file transfer to begin download of the file by the
+    //      requester
+    // rq_store_ref is used request to a peer nearby a given file hash to store
+    //      this node's fingerprint/file hash pair in their remote file table
+    //      optionally, the requestee can forward the fingerprint/hash pair to
+    //      any peers it knows even closer to the file
+    // rq_command is used by a client, and is always signed and encrypted
+    enum { rq_find_peer, rq_find_file, rq_store_ref, rq_command } type;
+    hash fingerprint;
+    byte *hmac; //TODO: figure out the best way for authentication
+                //      perhaps hmacs for all messages other than the response
+                //      to rq_find_peer that sends a peer's public key?
+    union {
+        struct {
+        } find_peer;
+        struct {
+        } find_file;
+        struct {
+        } store_ref;
+        struct {
+        } command;
+    };
+};
+
+void parse_message(buffer buf){
+    
 }
 
