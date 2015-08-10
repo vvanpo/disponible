ec75742578a266dfca870d0cc93320b49a2be503
Victor van Poppelen <vvanpo@gmail.com>
2015-08-10 16:19:23 -0400

    server.h: dir path determines a server instance

--- server.h
+++ server.h
@@ -4,14 +4,21 @@
 #include "dsp_error.h"
 
 enum dsp_srv_config {
-    DSP_SRV_DIR, DSP_SRV_BOOTSTRAP_FILE, DSP_SRV_KEYS_FILE,
-    DSP_SRV_BUCKET_SIZE, DSP_SRV_BUCKET_DEPTH_MAX,
-    DSP_SRV_PORT_SCTP, DSP_SRV_PORT_TCP, DSP_SRV_PORT_UDP,
+    DSP_SRV_DIR,
+    DSP_SRV_BOOTSTRAP_FILE,
+    DSP_SRV_KEYS_FILE,
+    DSP_SRV_AUTHORIZED_KEYS,
+    DSP_SRV_BUCKET_SIZE,
+    DSP_SRV_BUCKET_DEPTH_MAX,
+    DSP_SRV_PORT_SCTP,
+    DSP_SRV_PORT_TCP,
+    DSP_SRV_PORT_UDP,
 };
 
 int dsp_srv_setup(char *path);
-int dsp_srv_get_config(char **value, enum dsp_srv_config option);
-int dsp_srv_set_config(enum dsp_srv_config option, char *value);
-int dsp_srv_bootstrap();
+// *value is allocated and must be freed after use
+int dsp_srv_get_config(char **value, enum dsp_srv_config option, char *path);
+int dsp_srv_set_config(enum dsp_srv_config option, char *command, char *path);
+int dsp_srv_bootstrap(char *path);
 
 #endif
